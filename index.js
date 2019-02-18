c9edbeb4d81de5ca60e503dd5dd16b930dcb3722
Adam Wolf <adamjwolf1@gmail.com>
2019-02-18 03:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function johnLennonFacts() {
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
-        newFacts.push(facts[i] = "He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!");
+        newFacts.push(facts[i] = );
+        i++;
     }
     return newFacts;
 }
