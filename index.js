7eee13fcfcdaebcaa9facf854f3df08b8c2cd7e4
Adam Wolf <adamjwolf1@gmail.com>
2019-02-18 03:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function johnLennonFacts(array) {
   return array;
 }
 
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
